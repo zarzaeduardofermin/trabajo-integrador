5bb56901585ce7818642b7c51004df827e1d1f95
zarzaeduardofermin <103265319+zarzaeduardofermin@users.noreply.github.com>
2022-11-14 09:18:29 -0300

    Add files via upload

---
+++ integradora_PHP_HTML/buscar_empleados.php
@@ -0,0 +1,28 @@
+<?php
+$conn = new mysqli("localhost","root","","miempresa");
+$buscar = $_POST['buscar'];
+
+echo($buscar);
+echo("<br>");
+
+//consulta para hacer la busqueda de empleados
+$buscar = "SELECT empleados.legajo, empleados.apellido_nombre, empleados.fecha_ingreso,puestos.puesto as 'verPuesto',puestos.sueldo FROM empleados, puestos WHERE empleados.puesto = puestos.id AND empleados.apellido_nombre LIKE '%$buscar%'
+AND empleados.puesto = puestos.id";
+
+//ejecutamos la busqueda
+$query = mysqli_query($conn,$buscar);
+
+//foreach recorre todos los resultados obtenidos
+foreach($query as $res){
+    echo("<br>Legajo:". $res['legajo']);
+    echo("<br>Apellido y Nombres:". $res['apellido_nombre']);
+    echo("<br>Puesto: ".$res['verPuesto']);
+    echo("<br>Sueldo: $ ". $res['sueldo']);
+    echo("<br>Fecha de ingreso. ".$res['fecha_ingreso']);
+    echo("<br><a href='modificar.php'>Modificar</a>");
+    echo(" <a href='eliminar.php'>Eliminar</a>");
+    echo("<hr>");
+
+}
+?>
+
